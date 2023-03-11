2ab7414c7b5bd5a26ae1d35c8c6b4d54899a776b
shun-is-me <85098747+shun-is-me@users.noreply.github.com>
2023-03-11 17:43:33 +0900

    Delete 프로그래머스/lv0/120830. 양꼬치 directory

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++
@@ -1,9 +0,0 @@
-function solution(n, k) {
-    var answer = 0;
-    let re_n = Math.floor(n/10);
-    k = k - re_n;
-    console.log(k);
-    answer = (12000 * n)+(2000 * k);
-    console.log(answer);
-    return answer;
-}

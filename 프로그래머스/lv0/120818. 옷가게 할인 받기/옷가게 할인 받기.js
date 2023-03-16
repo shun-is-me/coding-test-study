88c66519d5a4ebdf570f0661ad66bd5ad270d491
shun-is-me <85098747+shun-is-me@users.noreply.github.com>
2023-03-16 23:31:24 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+function solution(price) {
+    let answer=0;
+    if(price>=500000){
+         answer =  price-(price*0.2)
+    }else if(price>=300000&&price<500000){
+          answer = price-(price*0.1)
+    }else if(price>=100000&&price<300000){
+       answer = price-(price*0.05)
+    }else{
+         answer = price
+    }
+     return Math.floor(answer)
+}
